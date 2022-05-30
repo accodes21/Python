b48674da698184cdf39cd5672daeff818b9156fd
Aarya Chopkar <aaryachopkar@gmail.com>
2022-05-30 15:40:00 +0530

    N terms

--- pattern2.py
+++ pattern2.py
@@ -8,7 +8,7 @@ for x in range(0,6):
         time.sleep(0.02)
     print("\r")
     
-for x in range(6,0,-1):
+for x in range(5,0,-1):
     for y in range(1,x+1):
         print(y, end="")
         time.sleep(0.02)
