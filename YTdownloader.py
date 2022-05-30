630c86bbbe4175d97c7183427b757b1c1221b6dd
Aarya Chopkar <aaryachopkar@gmail.com>
2022-05-30 15:41:58 +0530

    Youtube Video Downloader

---
+++ YTdownloader.py
@@ -0,0 +1,26 @@
+from pytube import YouTube
+link = input("Enter youtube url:")
+yt = YouTube(link)
+
+SAVE_PATH = "E:/"
+
+#Title of Video
+print("Title:",yt.title)
+
+#Number of Views
+print("Views:",yt.views)
+
+#Length of Video
+print("Length of Video:",yt.length,"seconds")
+
+#Rating of Video
+print("Rating:",yt.rating)
+
+print(yt.streams.filter(progressive=True))
+
+ys = yt.streams.get_by_itag('22')
+
+#Downloading
+print("***DOWNLOADING***")
+ys.download(SAVE_PATH)
+print("***DOWNLOADED***")
