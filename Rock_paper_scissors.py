e58a95ef289b62f70ba99e21556b67b4095bca0e
Aarya Chopkar <aaryachopkar@gmail.com>
2022-06-10 21:36:42 +0530

    Added loop

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -1,31 +1,37 @@
 import random
+while True:
 
-choices = ["rock","paper","scissors"]
-comp_choice = random.choice(choices)
+    choices = ["rock","paper","scissors"]
+    comp_choice = random.choice(choices)
 
-print("Welcome to Rock Paper Scissors Game!")
-user_choice = input("Enter your Choice \n(rock,paper,scissors): ")
+    print("Welcome to Rock Paper Scissors Game!")
+    user_choice = input("Enter your Choice \n(rock,paper,scissors): ")
 
-print("Your Choice:"+user_choice+" \nComputer Choice:"+comp_choice)
+    print("Your Choice:"+user_choice+" \nComputer Choice:"+comp_choice)
 
 
-if user_choice == comp_choice:
-    print("Its a tie")
-if user_choice == "rock":
-    if comp_choice == "scissors":
-        print("Rock smashes scissors! You win!")
-    else:
-        print("Paper covers rock! You lose.")
-    
-elif user_choice =="paper":
-        if comp_choice == "rock":
-            print("Paper covers rock! You win!")
+    if user_choice == comp_choice:
+        print("**Its a tie**")
+
+    if user_choice == "rock":   
+        if comp_choice == "scissors":
+            print("**Rock smashes scissors! You win!**")
         else:
-            print("Scissors cuts paper! You lose.")
+            print("**Paper covers rock! You lose.**")
+    
+    elif user_choice =="paper":
+        if comp_choice == "rock":
+            print("**Paper covers rock! You win!**")
+        else:
+            print("**Scissors cuts paper! You lose.**")
 
-elif user_choice =="scissors":
-    if comp_choice == "paper":
-        print("Scissors cuts paper! You win!")
-    else:
-        print("Rock smashes scissors! You lose.")
-        
+    elif user_choice =="scissors":
+        if comp_choice == "paper":
+            print("**Scissors cuts paper! You win!**")
+        else:
+            print("**Rock smashes scissors! You lose.**")
+
+    x = input("Do you want to play again? (y/n): ")
+    if x!= "y":
+        print("Thank You!")
+        break
